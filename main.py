c1fb5181c44ef6f065106ce26ee18bee857bba5f
Andile Mchunu <47420082+SkywalkingZulu@users.noreply.github.com>
2021-01-04 08:11:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#Selenium Webdriver must be installed for this to work
+
+import time;
+from selenium import webdriver;
+
+#time to refresh page (seconds)
+Timer = 120
+
+#youtube link
+link = 'https://youtu.be/pkGPnMSX_Woautoplay=1&amp;'
+
+#number of views
+views = 20
+
+driver = webdriver.Chrome()
+driver.get(link)
+
+for i in range(views):
+    time.sleep(Timer)
+    driver.refresh()
+    print(i)
